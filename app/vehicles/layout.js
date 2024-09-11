d466ed1af1c9c57a1e25ad0d96254de56c2d0f89
Carlos Ali Garcia <cgarcia@evidation.com>
2024-09-11 03:36:11 -0500

    Create layouts

---
+++ app/vehicles/layout.js
@@ -0,0 +1,8 @@
+export default function VehiclesLayout({ children }) {
+  return (
+    <div>
+      <h1>My Vehicles</h1>
+      {children}
+    </div>
+  );
+}
---
+++ app/vehicles/trucks/page.js
@@ -0,0 +1,3 @@
+export default function Trucks() {
+  return <h2>Trucks</h2>;
+}
